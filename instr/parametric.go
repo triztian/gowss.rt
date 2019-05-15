b462d9b8508fa7bad95869b8468e6df46f743be4
Tristian Azuara <tristian.azuara@gmail.com>
2019-05-14 22:19:41 -0700

    Add parametric instructions opcodes

---
+++ instr/parametric.go
@@ -0,0 +1,8 @@
+package instr
+
+// Parametric
+// https://webassembly.github.io/spec/core/bikeshed/index.html#parametric-instructions%E2%91%A0
+const (
+	Drop   OpCode = 0x1A
+	Select        = 0x1B
+)
